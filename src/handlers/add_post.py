36a17828be664f86ccb2d61cd17a33a406cddf1f
“david <davzy007@gmail.com>
2025-05-23 17:12:09 +0100

    clean up

--- src/handlers/add_post.py
+++ src/handlers/add_post.py
@@ -11,37 +11,54 @@ def get_table():
     dynamodb = boto3.resource('dynamodb')
     return dynamodb.Table(os.environ['TABLE_NAME'])
 
-def handler(event, context, table=None):
+def handler(event, _, table=None):
     if table is None:
+        # get table if none is parsed (for testing)
         table = get_table()
 
     try:
-        body = json.loads(event['body'])
-        post_id = str(uuid.uuid4())
+        body = json.loads(event['body'])  # loads content of the post body
+        post_id = str(uuid.uuid4())  # generates random uuid
+        post_message = str(body['message'])
+        post_sender: str = body['sender']
+        post_date = str(datetime.now())  # gets current date and time
 
-        post_date = str(datetime.now())
+        if post_message is None or post_sender is None:
+            # if post data is incomplete
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'error': 'Post data is incomplete.'
+                })
+            }
+
+        # create new post object to send to db
         new_post = {
             'id': post_id,
-            'message': body['message'],
-            'sender': body['sender'],
+            'message': post_message,
+            'sender': post_sender,
             'date_added': post_date
         }
-        table.put_item(Item=new_post)
+        table.put_item(Item=new_post)  # add new post to db
         return {
+            # everything is okay :)
             'statusCode': 201,
             'body': json.dumps({
-                'message': f'Added new post with id: {post_id}'
+                'message': post_id
             })
         }
     except (json.JSONDecodeError, KeyError):
+        # if there is a problem decoding the json parsed in request body
         return {
             'statusCode': 400,
             'body': json.dumps({
-                'error': 'Post data is incomplete.'
+                'error': 'Invalid request body.'
             })
         }
 
     except ClientError as e:
+        # somthing has gone terribly wrong :(
+        print(e)  # print the error message and don't send it to client
         return {
             'statusCode': 500,
             'body': json.dumps({
