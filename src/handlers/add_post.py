ec8002c18ccaab1ba8ae536c1f792152995e2aa6
“david <davzy007@gmail.com>
2025-05-23 18:49:51 +0100

    nope changed my mind

--- src/handlers/add_post.py
+++ src/handlers/add_post.py
@@ -11,15 +11,6 @@ def get_table():
     dynamodb = boto3.resource('dynamodb')
     return dynamodb.Table(os.environ['TABLE_NAME'])
 
-def return_json(status_code: int, message: dict[str:str]):
-    return {
-        'statusCode': status_code,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': json.dumps(message)
-    }
-
 def handler(event, _, table=None):
     if table is None:
         # get table if none is parsed (for testing)
@@ -34,9 +25,12 @@ def handler(event, _, table=None):
 
         if len(post_message.strip()) < 1 or len(post_sender.strip()) < 2:
             # if post data is incomplete
-            return_json(400, {
-                'error': 'Invalid request body.'
-            })
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'error': 'Invalid request body.'
+                })
+            }
 
         # create new post object to send to db
         new_post = {
@@ -46,19 +40,28 @@ def handler(event, _, table=None):
             'date_added': post_date
         }
         table.put_item(Item=new_post)  # add new post to db
-        return_json(201, {
-            'message': post_id
-        })
+        return {
+            # everything is okay :)
+            'statusCode': 201,
+            'body': json.dumps({
+                'message': post_id
+            })
+        }
     except (json.JSONDecodeError, KeyError):
         # if there is a problem decoding the json parsed in request body
-        return_json(400, {
-            'error': 'Invalid request body.'
-        })
-
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'error': 'Invalid request body.'
+            })
+        }
 
     except ClientError as e:
         # somthing has gone terribly wrong :(
         print(e)  # print the error message and don't send it to client
-        return_json(500, {
-            'error': 'Somthing went wrong, try again.'
-        })
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'error': 'Somthing went wrong, try again.'
+            })
+        }
