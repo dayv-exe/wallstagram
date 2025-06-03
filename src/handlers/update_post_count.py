1172e0301d188c7c6fc144c25c66bdb5254770aa
“david <davzy007@gmail.com>
2025-06-03 16:06:26 +0100

    can no longer update post count if user doesn't exist

--- src/handlers/update_post_count.py
+++ src/handlers/update_post_count.py
@@ -7,21 +7,31 @@ from shared.User import User
 
 
 def get_table():
+    # injects table
     dynamodb = boto3.resource('dynamodb')
     return dynamodb.Table(os.environ['TABLE_NAME'])
 
+def user_exists(username: str, table) -> bool:
+    response = table.get_item(Key= User.database_key(username))
+    user = response.get('Item')
+    return user is not None
+
 def handler(event, context, table=None):
     if table is None:
+        # allows function to be tested without a real dynamodb table
         table = get_table()
 
     for record in event['Records']:
         try:
-            body = json.loads(record['body'])
-            username = body['username']
-            increase_count = body['increase_count']
-            if username is None or increase_count is None:
-                raise ClientError(error_response='One or more parameters are missing', operation_name="update post count")
 
+            body = json.loads(record['body'])  # extracts body from pipeline or whatever
+            username = body['username']  # gets username
+            increase_count = body['increase_count']  # gets whether we will increase count on user data
+            if username is None or increase_count is None or not user_exists(username, table):
+                # if data from sqs is incomplete
+                continue
+
+            # to actually update the table
             table.update_item(
                 Key=User.database_key(username),
                 UpdateExpression='ADD post_count :inc' if increase_count else 'ADD post_count :dec',
